49a77e62de733af8382803dbb1f4300493c8efcf
aryndraa <aryacoder1102@gmail.com>
2025-05-06 12:12:26 +0800

    menu category

--- transporter-app/app/Models/Menu.php
+++ transporter-app/app/Models/Menu.php
@@ -3,19 +3,26 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 
 class Menu extends Model
 {
     protected $fillable = [
         'name',
+        'category_id',
         'stock',
         'price',
-        'photo'
+        'photo',
     ];
 
     public function menuAdditional(): BelongsToMany
     {
         return $this->belongsToMany(Additional::class, 'menu_additional', 'menu_id', 'additional_id');
+    }
+
+    public function category(): BelongsTo
+    {
+        return $this->belongsTo(MenuCategory::class);
     }
 }
