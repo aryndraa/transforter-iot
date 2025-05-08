bd3db8aaa77c27ce570f8f194aaac24ff9f06fee
aryndraa <aryacoder1102@gmail.com>
2025-05-08 08:42:46 +0800

    menu additional table

---
+++ web/dashboard/database/migrations/2025_05_08_004212_create_menu_additional_table.php
@@ -0,0 +1,29 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('menu_additional', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('menu_id')->constrained('menus');
+            $table->foreignId('additional_id')->constrained('additional');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('menu_additional');
+    }
+};
