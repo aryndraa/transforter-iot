8d690d670258b2c384ca7bf75398d4a06f069f83
aryndraa <aryacoder1102@gmail.com>
2025-05-08 09:13:23 +0800

    payment table

--- web/dashboard/app/Models/Order.php
+++ web/dashboard/app/Models/Order.php
@@ -38,4 +38,9 @@ class Order extends Model
     {
         return $this->hasMany(OrderItem::class);
     }
+
+    public function payments(): HasMany
+    {
+        return $this->hasMany(Payment::class);
+    }
 }
