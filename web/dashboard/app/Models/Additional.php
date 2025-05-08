2eb948213c21f84c971dbbc1b18f4579bcd2f270
aryndraa <aryacoder1102@gmail.com>
2025-05-08 08:39:30 +0800

    additional variant model

--- web/dashboard/app/Models/Additional.php
+++ web/dashboard/app/Models/Additional.php
@@ -3,6 +3,7 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class Additional extends Model
 {
@@ -10,4 +11,9 @@ class Additional extends Model
         'name',
         'description',
     ];
+
+    public function variants(): HasMany
+    {
+        return $this->hasMany(AdditionalVariant::class);
+    }
 }
--- web/dashboard/app/Models/AdditionalVariant.php
+++ web/dashboard/app/Models/AdditionalVariant.php
@@ -3,14 +3,22 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 
-class Variant extends Model
+class AdditionalVariant extends Model
 {
     protected $fillable = [
+        'additional_id',
         'name',
         'image',
         'additional_price',
     ];
+
+    public function additional(): BelongsTo
+    {
+        return $this->belongsTo(Additional::class);
+    }
+
 
 }
